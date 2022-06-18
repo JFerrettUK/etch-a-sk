40b5c3b10c42152538dc77a8338fdee15d6bef74
JFerrettUK <jferrettuk@gmail.com>
2022-06-18 12:32:27 +0100

    Stylesheet and strcture to EaS

---
+++ js-file.js
@@ -0,0 +1,17 @@
+// Create 16x16 DIV boxes
+
+ 
+var container = document.getElementById("grid");
+
+var box1 = document.createElement("div");
+box1.innerHTML = "TEXT";
+container.appendChild(box1);
+
+var box2 = document.createElement("div");
+box2.innerHTML = "TEXT";
+container.appendChild(box2);
+
+var box3 = document.createElement("div");
+box3.innerHTML = "TEXT";
+container.appendChild(box3);
+
