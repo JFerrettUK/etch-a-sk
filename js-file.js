dda1c3920917b40721373f14e828737e81a401c0
JFerrettUK <jferrettuk@gmail.com>
2022-06-20 09:36:40 +0100

    Etch-a-sk

--- js-file.js
+++ js-file.js
@@ -4,10 +4,21 @@ const container = document.getElementById("container");
 
 for(x=0; x<256; x++) {
     var divBox = document.createElement("div");
+    divBox.className = "divBox";
     document.getElementById("container").appendChild(divBox);
 }
 
-container.addEventListener("click", backgroundChange);
-function backgroundChange() {
-    document.body.style.backgroundColor = 'salmon';
+const boxSel = document.getElementsByClassName("divBox");
+
+boxSel.onmouseover = logMouseOver;
+boxSel.onmouseout = logMouseOut;
+function logMouseOver() {
+    container.style.backgroundColor = 'yellow';
+    console.log("mouseover")
 }
+  
+function logMouseOut() {
+    container.style.backgroundColor = 'transparent';
+    console.log("mouseover")
+  }
+  
